d196080992a3694262480b4f2c2ef39086bb1a1f
Irenitemi Agbejule <47606100+Dart120@users.noreply.github.com>
2024-06-16 17:34:55 +0100

    Moving to vercel

--- netlify/functions/api.py
+++
@@ -1,6 +0,0 @@
-import awsgi
-from api.app import app  # Import the Flask app
-
-def handler(event, context):
-    """ This function will be invoked by Netlify when the function endpoint is accessed. """
-    return awsgi.response(app, event, context)
